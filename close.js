531e3a675a60b4d3b21f2fdb49b4028f6c0bc371
warisha-jahan <officialwarishajahanzaib@gmail.com>
2024-09-01 16:57:44 +0500

    added intial work

---
+++ close.js
@@ -0,0 +1,4 @@
+document.getElementById('humberger').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.getElementById('menu').classList.toggle('active');
+});
