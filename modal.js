c81dadac94d465eafa575b125a0449e06654324a
warisha-jahan <officialwarishajahanzaib@gmail.com>
2024-09-19 15:01:52 +0500

    added some js

---
+++ modal.js
@@ -0,0 +1,25 @@
+// Get the modal
+var modal = document.getElementsByClassName("contactMe")[0];
+
+// Get all the cards and the close button
+var cards = document.querySelectorAll(".price-card");
+var closeBtn = document.getElementsByClassName("close")[0];
+
+// Loop through all the cards and add the click event to each
+cards.forEach(function(card) {
+    card.onclick = function() {
+        modal.style.display = "flex";
+    }
+});
+
+// When the user clicks on the close button, close the modal
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks outside the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
