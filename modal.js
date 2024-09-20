347dd1c6bfdce25ace5ce6c1468433872f1802d8
warisha-jahan <officialwarishajahanzaib@gmail.com>
2024-09-20 18:31:26 +0500

    added js and css for form popup

--- modal.js
+++ modal.js
@@ -9,17 +9,23 @@ var closeBtn = document.getElementsByClassName("close")[0];
 cards.forEach(function(card) {
     card.onclick = function() {
         modal.style.visibility = "visible";
+        modal.style.opacity="1"
+        modal.style.transform="scale(1.1)"
     }
 });
 
 // When the user clicks on the close button, close the modal
 closeBtn.onclick = function() {
     modal.style.visibility = "hidden";
+     modal.style.opacity="0";
+     modal.style.transform="scale(0.3)";
 }
 
 // When the user clicks outside the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.visibility = "hidden";
+         modal.style.opacity="0"
+         modal.style.transform="scale(0.3)";
     }
 }
